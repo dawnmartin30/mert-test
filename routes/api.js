c98af0cb66b27b5918074c741ee2d8bd2d9070dd
Joseph Finfrock <70216057+4804032140@users.noreply.github.com>
2023-10-20 16:40:17 -0400

    Unduing temp

--- routes/api.js
+++ routes/api.js
@@ -38,4 +38,36 @@ router.post('/login', async (req, res, next) => {
   await client.close();
 });
 
+router.post('/register', async (req, res, next) => {
+  const client = await MongoClient.connect(Need to replace this with a enviroment variable I will get this fixed soon tm);
+  const database = client.db('COP4331');
+  const collection = database.collection('Users');
+  const users = await collection.find({ Username: req.body.username}).toArray();
+  if (users.length > 0) {
+    res.json({
+      err: "Username is taken!"
+    });
+    await client.close();
+    return;
+  }
+  const id = new ObjectId();
+  const date = new Date();
+  await collection.insertOne({
+    _id: id,
+    FirstName: req.body.firstname,
+    LastName: req.body.lastname,
+    Username: req.body.username,
+    Password: req.body.password,
+    DateCreated: date,
+    DateLastLoggedIn: "",
+    Email: req.body.email,
+    Ingredients: [],
+    Badges: []
+  })
+  res.json({
+      msg: "User registered"
+  });
+  await client.close();
+});
+
 module.exports = router;
