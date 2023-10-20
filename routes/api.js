874de2d5f793a663832b5c4bdc8afa96ca3a35a0
Joseph Finfrock <70216057+4804032140@users.noreply.github.com>
2023-10-20 16:47:20 -0400

    Cleaned up api.js to get rid of dummy endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Todo = require('../models/todo');
-
-router.get('/get', (req, res, next) => {
-  // This will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
-    .then((data) => res.json(data))
-    .catch(next);
-});
-
-router.post('/make', (req, res, next) => {
-  if (req.body.action) {
-    Todo.create(req.body)
-      .then((data) => res.json(data))
-      .catch(next);
-  } else {
-    res.json({
-      error: 'The input field is empty',
-    });
-  }
-});
-
-router.delete('/delete/:id', (req, res, next) => {
-  Todo.findOneAndDelete({ _id: req.params.id })
-    .then((data) => res.json(data))
-    .catch(next);
-});
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+require('dotenv').config();
 
 router.post('/login', async (req, res, next) => {
   const client = await MongoClient.connect(process.env.DB);
